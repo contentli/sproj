c3b965fa72fb5974dc620b150b9870bae3bbc0bf
Kjell-Magnus Vike <magnus@vike.se>
2019-05-01 23:06:51 +0200

    Refactored ProductController

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Dashboard;
 
 use App\Product;
 use App\Category;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller

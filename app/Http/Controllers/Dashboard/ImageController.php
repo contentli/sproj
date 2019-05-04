b0d9e66e3488f4bea25bd574618f24dcea78428b
Kjell-Magnus Vike <magnus@vike.se>
2019-05-04 21:40:19 +0200

    Added security

--- app/Http/Controllers/Dashboard/ImageController.php
+++ app/Http/Controllers/Dashboard/ImageController.php
@@ -6,8 +6,26 @@ use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+
 class ImageController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Upload a image
+     *
+     * @param [type] $product_id
+     * @param [type] $file_path
+     * @return void
+     */
     public function upload($product_id, $file_path)
     {
         $product = Product::find($product_id);

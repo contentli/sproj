d2c529d675df57ae5bd33821eff37719d76e94db
Magnus Vike <magnus@vike.se>
2019-05-06 13:17:56 +0200

    Update footer.blade.php

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,7 +2,8 @@
     <div class="container">
         <div class="content">
             <p>
-                <a href="#">Conditions of Use</a> <a href="#">Privacy Notice</a> © 2019, LeetMark.
+                {{-- <a href="#">Conditions of Use</a> <a href="#">Privacy Notice</a> © 2019, LeetMark. --}}
+                © 2019, LeetMark.
             </p>
         </div>
     </div>

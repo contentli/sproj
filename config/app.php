350bf85469d835710948a9d394708778651e75d4
Magnus Vike <magnus@vike.se>
2019-05-06 11:16:42 +0200

    Added ViewServiceProvider

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Leetmark'),
+    'name' => env('APP_NAME', 'LeetMark'),
 
     /*
     |--------------------------------------------------------------------------
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ViewServiceProvider::class,
 
     ],
 

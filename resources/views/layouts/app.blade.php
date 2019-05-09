456470828e4946f6d272c4e9af324af0b18c28e6
Kjell-Magnus Vike <magnus@vike.se>
2019-05-09 22:57:45 +0200

    Added GA tracking code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,17 @@
         <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
         <meta name="description" content="@yield('meta_description', 'A curated list of only the best and popular products')">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>@yield('title' , 'Home') - {{ config('app.name', 'LeetMark') }}</title>
-
-        <!-- Styles -->
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139938954-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-139938954-1');
+        </script>
+
     </head>
     <body itemscope itemtype="http://schema.org/WebPage">
         <div id="app">
